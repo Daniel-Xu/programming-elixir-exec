6631a56efabc1070b238e3db30193c53280d068c
danielxu <xwd.forever@gmail.com>
2016-07-25 16:32:02 -0400

    add randomness to prove ^ works

--- part2/pmap.ex
+++ part2/pmap.ex
@@ -1,10 +1,16 @@
 defmodule Parallel do
+  import :timer, only: [ sleep: 1 ]
+
   def pmap(collection, fun) do
     current = self
     collection
     |> Enum.map(
       fn(elem) ->
-        spawn_link(fn -> send(current, {self, fun.(elem)}) end)
+        spawn_link(
+          fn ->
+            sleep Enum.random([1, 2, 3]) * 100
+            send(current, {self, fun.(elem)})
+          end)
       end)
 
     |> Enum.map(
