3bfb82594cdfb5b311bc4f865a36b4302e9b3ad1
danielxu <xwd.forever@gmail.com>
2016-08-18 14:10:13 -0400

    server process using GenServer

---
+++ otp/counter_genserver.exs
@@ -0,0 +1,31 @@
+defmodule Counter do
+  use GenServer
+
+  def inc(pid), do: GenServer.cast(pid, :inc)
+
+  def dec(pid), do: GenServer.cast(pid, :dec)
+
+  def val(pid) do
+    GenServer.call(pid, :val)
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
+
+  def start_link(initial_val) do
+    GenServer.start_link(__MODULE__, initial_val)
+  end
+
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
+end
